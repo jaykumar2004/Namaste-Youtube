6afabf01082c2a38cba604859cab85d8440514c0
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-06-09 18:01:33 +0530

    youtube

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice.jsx";
 import { useSearchParams } from "react-router-dom";
 import CommentsContainer from "./CommentsContainer.jsx";
+import LiveChat from "./LiveChat.jsx";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams({});
@@ -13,20 +14,25 @@ const WatchPage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col">
-      <div className="px-5">
-        <iframe
-          width="800"
-          height="400"
-          src={`https://www.youtube.com/embed/${searchParams.get(
-            "v"
-          )}?autoplay=0`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+    <div className="flex flex-col w-full">
+      <div className="px-5 flex">
+        <div>
+          <iframe
+            width="800"
+            height="400"
+            src={`https://www.youtube.com/embed/${searchParams.get(
+              "v"
+            )}?autoplay=0`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="w-full">
+          <LiveChat/>
+        </div>
       </div>
       <CommentsContainer />
     </div>
--- src/utils/store.jsx
+++ src/utils/store.jsx
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import appSlice from "./appSlice.jsx";
+import chatSlice from "./chatSlice.jsx"
 
 const store = configureStore({
     reducer: {
         app: appSlice,
+        chat: chatSlice
     }
 });
 
